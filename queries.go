7e7a97e560277e61353b02152e064ee989a83375
w7n <73303644+w7o@users.noreply.github.com>
2026-07-05 14:51:54 -0400

    Split queries into their own file, added poke_queries

---
+++ queries.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"log"
+	// "encoding/json"
+)
+
+func json_query(endpoint string)(string, error){
+	log.SetPrefix("json_query: ")
+	log.SetFlags(0)
+	
+	//Empty string check
+	if (endpoint == "") {
+		return "", errors.New("Empty endpoint")
+	}
+
+	//Send request to endpoint to get a response
+	log.Printf("Request to %v",endpoint)
+	response, err := http.Get(endpoint)
+
+	//Error check
+	if err != nil {
+		return "", err
+	}
+
+	//Read the response's data
+	respData, err := io.ReadAll(response.Body)
+
+
+	//Error check
+	if err != nil {
+		return "", err
+	}
+	
+	return string(respData), nil
+}
+
+func poke_query(id string)(string, error){ 
+	endpoint := "https://pokeapi.co/api/v2/pokemon/" + id
+	return json_query(endpoint)
+}
---
+++ types.go
@@ -0,0 +1,5 @@
+/*
+Comments marked with $ are notes for myself in the process of learning Golang
+*/
+
+package main
