fa1e4df00030093aa391c7ddc1c3758eb2174dd3
w7n <w7o@users.noreply.github.com>
2026-07-14 15:04:15 -0400

    Revamped poke_query to return a struct instead of the JSON itself
    And also added some better error returns

--- queries.go
+++ queries.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	// "encoding/json"
 )
 
-func json_query(endpoint string) (string, error) {
+func json_query(endpoint string) ([]byte, error) {
 	log.SetPrefix("json_query: ")
 	log.SetFlags(0)
 
 	//Empty string check
 	if endpoint == "" {
-		return "", errors.New("Empty endpoint")
+		return nil, errors.New("Empty endpoint")
 	}
 
 	//Send request to endpoint to get a response
@@ -23,21 +25,42 @@ func json_query(endpoint string) (string, error) {
 
 	//Error check
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("request %q failed: %w", endpoint, err)
 	}
 
+	//Close request
+	defer response.Body.Close()
+
 	//Read the response's data
-	respData, err := io.ReadAll(response.Body)
+	data, err := io.ReadAll(response.Body)
 
 	//Error check
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("read response body failed: %w", err)
 	}
 
-	return string(respData), nil
+	return data, nil
 }
 
-func poke_query(id string) (string, error) {
+/*
+Queries the API and returns a struct containing all exported information
+*/
+func poke_query(id string) (PokemonSpecies, error) {
 	endpoint := SITE + "pokemon/" + id
-	return json_query(endpoint)
+	// Grab JSON data using endpoint
+	data, err := json_query(endpoint)
+
+	if err != nil {
+		return PokemonSpecies{}, fmt.Errorf("JSON query failed: %w", err)
+	}
+
+	// Unpack the JSON data into a PokemonSpecies struct
+	var pokemon PokemonSpecies
+	json.Unmarshal(data, &pokemon)
+
+	if err != nil {
+		return PokemonSpecies{}, fmt.Errorf("JSON unmarshal for Pokémon %q failed: %w", id, err)
+	}
+
+	return pokemon, nil
 }
