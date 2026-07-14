759b26c7e7b3d95086b28cb6029e71e07276d483
w7n <w7o@users.noreply.github.com>
2026-07-14 14:39:33 -0400

    Implemented abilites/moves structs for unmarshalling

--- queries.go
+++ queries.go
@@ -3,22 +3,22 @@ package main
 import (
 	"errors"
 	"io"
-	"net/http"
 	"log"
+	"net/http"
 	// "encoding/json"
 )
 
-func json_query(endpoint string)(string, error){
+func json_query(endpoint string) (string, error) {
 	log.SetPrefix("json_query: ")
 	log.SetFlags(0)
 
 	//Empty string check
-	if (endpoint == "") {
+	if endpoint == "" {
 		return "", errors.New("Empty endpoint")
 	}
 
 	//Send request to endpoint to get a response
-	log.Printf("Request to %v",endpoint)
+	log.Printf("Request to %v", endpoint)
 	response, err := http.Get(endpoint)
 
 	//Error check
@@ -29,16 +29,15 @@ func json_query(endpoint string)(string, error){
 	//Read the response's data
 	respData, err := io.ReadAll(response.Body)
 
-
 	//Error check
 	if err != nil {
 		return "", err
 	}
-	
+
 	return string(respData), nil
 }
 
-func poke_query(id string)(string, error){ 
+func poke_query(id string) (string, error) {
 	endpoint := SITE + "pokemon/" + id
 	return json_query(endpoint)
 }
--- types.go
+++ types.go
@@ -10,32 +10,67 @@ Prefix G
 */
 type GrowthClass int
 
-const(
-	G_MEDIUM_FAST		GrowthClass = iota + 1
+const (
+	G_MEDIUM_FAST GrowthClass = iota + 1
 	G_ERRATIC
 	G_FLUCTUATING
 	G_MEDIUM_SLOW
 	G_FAST
 	G_SLOW
-	G_SLIGHT_FAST		GrowthClass = 16
-	G_SLIGHT_SLOW		GrowthClass = 17
+	G_SLIGHT_FAST GrowthClass = 16
+	G_SLIGHT_SLOW GrowthClass = 17
 )
 
+/*
+Represents a generic object with a name and URL to that object's information
+*/
+type NamedResource struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+/*
+Represents an ability's species-specific
+properties:
+• Whether or not it's a hidden ability
+• Which slot the ability is in (is this even important)
+*/
+type PokemonAbility struct {
+	Ability  NamedResource `json:"ability"`
+	IsHidden bool          `json:"is_hidden"`
+	Slot     int           `json:"slot"`
+}
+
+/*
+Represents a move's species-specific
+properties, though none exist at the time of implementation
+*/
+type PokemonMove struct {
+	Move NamedResource `json:"move"`
+}
 
 /*
 This struct defines a Pokémon's basic statistics derived from their species
 */
-type PokemonSpecies struct{
-	Name		string		`json:"name"`		// Name of the Pokémon
-	FormFlag	int								// Form number of the Pokémon (Default: 0)
-	Height		int			`json:"height"`		// Height of the Pokémon, in 0.1kg
-	Weight		int			`json:"weight"`		// Weight of the Pokémon, in 0.1kg
+type PokemonSpecies struct {
+	Name      string           `json:"name"`            // Name of the Pokémon
+	Height    int              `json:"height"`          // Height of the Pokémon, in 0.1kg
+	Weight    int              `json:"weight"`          // Weight of the Pokémon, in 0.1kg
+	BaseEXP   int              `json:"base_experience"` // Base experience yield from defeating this Pokémon
+	Abilities []PokemonAbility `json:"abilities"`       // Available abilities of a Pokémon (up to 3)
+	Moves     []PokemonMove    `json:"moves"`           // Available moves of a Pokémon across all generations
 
-	BHappiness	int								// Default happiness, max 255
-	CaptureRate	int								// Species-dependent capture probability
-	GrowthRate	GrowthClass						// Determines how much XP needed per level
-	
+	FormFlag int // Form number of the Pokémon (Default: 0)
 
-	Color		string							// Cosmetic color used within the Pokédex
-	Generation	int								// Generation of origin
+	BHappiness  int         // Default happiness, max 255
+	CaptureRate int         // Species-dependent capture probability
+	GrowthRate  GrowthClass // Determines how much XP needed per level
+
+	Color      string // Cosmetic color used within the Pokédex
+	Generation int    // Generation of origin
+
+	/*
+		Add in future?
+		• Held Items
+	*/
 }
