3edb3fc62918ebcf292940ae21a81024c1a1ab95
w7n <w7o@users.noreply.github.com>
2026-07-14 15:07:32 -0400

    main.go now handles and prints the new struct

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ var SITE string = "https://pokeapi.co/api/v2/"
 
 func main() {
 	// if $env:DEV="1" (in powershell) then use locally stored instead
-	if os.Getenv("DEV") == "1"{
+	if os.Getenv("DEV") == "1" {
 		SITE = "http://localhost:8080/"
 	}
 
@@ -18,19 +18,20 @@ func main() {
 	//Format: Ldate, Ltime, Lshortfile
 	log.SetFlags(0b11001000)
 
-	pokemon := "197"
+	// testing variable
+	pokemonID := "197"
 
-	data, err := poke_query(pokemon)
+	data, err := poke_query(pokemonID)
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Print(data[:111])
+	fmt.Printf("%+v\n", data)
 }
 
 /* important commands:
 go build: creates executable
 go get (external link): imports external package and updates go.mod and go.sum
-go test -v ./...: test there 
+go test -v ./...: test there
 */
