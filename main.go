153644842cf27f2ec1adba7ef76c2b7fa8204403
w7n <73303644+w7o@users.noreply.github.com>
2026-08-08 22:55:46 -0400

    [v0.0.8] Updated design documents

--- main.go
+++ main.go
@@ -33,9 +33,9 @@ import (
 	"github.com/davecgh/go-spew/spew"
 )
 
-var IS_DEV_BUILD bool = true // must be set to false for every version bump
+var IS_DEV_BUILD bool = false // must be set to false for every version bump
 var SITE string = "https://pokeapi.co/api/v2/"
-var VERSION string = "0.0.7"
+var VERSION string = "0.0.8"
 var PROJECT_NAME string = "Poké7DB"
 
 func generateVersionNumber() {
