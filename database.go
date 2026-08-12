7ae6b30d9f069b65a202bf484c323076ac1fff1d
w7n <w7o@users.noreply.github.com>
2026-08-12 12:15:46 -0400

    Seeding for lookup tables has been implemented

--- database.go
+++ database.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	// "context"
+
+	"bytes"
 	"database/sql"
+	"encoding/csv"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 
 	_ "modernc.org/sqlite"
 )
@@ -148,6 +156,134 @@ func generateTableQuery(tableName, colBody, colMetadata string) (string, error) 
 	`, tableName, colBody, colMetadata, colConstraint), nil
 }
 
+func seedTable(tx *sql.Tx, csvPath string, sqlPath string, tableIndex int, timestamp string) error {
+	// Condition to exclude metadata
+	includeMetadataCondition := tableIndex != 0
+
+	csvContents, err := os.ReadFile(csvPath)
+	if err != nil {
+		return retError("D_11", "Failed to obtain .csv file", err)
+	}
+
+	sqlContents, err := os.ReadFile(sqlPath)
+	if err != nil {
+		message := fmt.Sprintf("csv -> sql file \"%s\" doesn't appear to exist", sqlPath)
+		return retError("D_21", message, err)
+	}
+
+	tableName, err := grabTableName(string(csvContents), "csv")
+	if err != nil {
+		return retError("D_12", "Failed to grab table name csv", err)
+	}
+
+	includeID, err := checkIncludeID(string(sqlContents), "sql")
+	if err != nil {
+		return retError("D_20", "Failed to grab ID inclusion bool sql", err)
+	}
+
+	csvFile, _ := os.Open(csvPath)
+
+	// Create a new reader for the CSV file
+	reader := csv.NewReader(csvFile)
+	reader.Comment = '#'
+
+	// CSV Header
+	columns, err := reader.Read()
+	if err != nil {
+		return retError("D_13", "Failed to read CSV header", err)
+	}
+
+	// Include createdAt timestamp if table contains metadata
+	// NOTE: If editing to include more seeded metadata in the future, check
+	// all includeMetadataCondition usages
+	const userMetadataSeedValuesCount int = 2
+
+	if includeMetadataCondition {
+		columns = append(
+			append([]string{}, columns...),
+			"createdAt",
+			"enabled",
+		)
+		if includeID {
+			columns = append(
+				append([]string{}, columns...),
+				"ID",
+			)
+		}
+	}
+
+	var placeholders []string
+	for range columns {
+		placeholders = append(placeholders, "?")
+	}
+
+	// Ready a placeholder query
+	query := fmt.Sprintf(
+		"INSERT INTO %s (%s) VALUES (%s) ON CONFLICT DO NOTHING",
+		tableName,
+		strings.Join(columns, ", "),
+		strings.Join(placeholders, ", "),
+	)
+	writeLog(fmt.Sprintf("DB: Query -  %s", query))
+	statement, err := tx.Prepare(query)
+	if err != nil {
+		return retError("D_15", "Placeholder query preparation failed", err)
+	}
+
+	defer statement.Close()
+
+	// repeat until break (end of file)
+	for {
+		row, err := reader.Read()
+
+		// end of file
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return retError("D_14", "Failed to read CSV row", err)
+		}
+
+		// row/column count sanity check
+		expectedValuesCount := len(columns)
+		if includeMetadataCondition {
+			expectedValuesCount -= userMetadataSeedValuesCount
+			if includeID {
+				expectedValuesCount -= 1
+			}
+		}
+		if len(row) != expectedValuesCount {
+			message := fmt.Sprintf("CSV row has %d values, expected %d", len(row), expectedValuesCount)
+			return retError("D_16", message, err)
+		}
+
+		args := strToAny(row)
+
+		if includeMetadataCondition {
+			// ..., createdAt, enabled
+			args = append(args, timestamp, 1)
+			if includeID {
+				args = append(args, args[0])
+			}
+		}
+
+		writeLog(fmt.Sprintf("DB: Arguments - %v", args))
+
+		r, err := statement.Exec(args...)
+		if err != nil {
+			return retError("D_17", "Executing statement failed", err)
+		}
+
+		rowsAffected, err := r.RowsAffected()
+		if err != nil {
+			return retError("D_18", "Failed to get rows affected", err)
+		}
+
+		writeLog(fmt.Sprintf("DB: %d row[s] affected", rowsAffected))
+	}
+	return nil
+}
+
 func databasePing(database *sql.DB) error {
 	// Test that the database works
 	err := database.Ping()
@@ -250,6 +386,66 @@ func databaseCreateLookup(database *sql.DB) error {
 	return tx.Commit()
 }
 
+func databaseSeedLookup(database *sql.DB) error {
+	log.Print("DB: Seeding lookup tables")
+	err := databasePing(database)
+	if err != nil {
+		return err
+	}
+
+	tx, err := database.Begin()
+	if err != nil {
+		return retError("D_08", "Database transaction init failed", err)
+	}
+	defer tx.Rollback()
+
+	var seedDir string = "./database/seed/00_lookup"
+	var schemaDir string = "./database/schema_columns/00_lookup"
+
+	files, err := os.ReadDir(seedDir)
+	if err != nil {
+		return retError("D_09", "Reading seed lookup directory failed", err)
+	}
+
+	var re_fileNameIndex *regexp.Regexp = regexp.MustCompile("^([0-9]{2})_.+$")
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		if filepath.Ext(file.Name()) != ".csv" {
+			continue
+		}
+		sqlFileName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name())) + ".sql"
+
+		matches := re_fileNameIndex.FindStringSubmatch(file.Name())
+
+		if len(matches) < 2 {
+			log.Printf("DB: Notice: Ignoring CSV file in lookup %s with incorrect filename format", file.Name())
+			continue
+		}
+
+		// convert obtained index to int
+		s, err := strconv.Atoi(matches[1])
+		if err != nil {
+			return retError("D_10", "Failed to convert an index to int", err)
+		}
+
+		// obtain the csv file
+		csvPath := filepath.Join(seedDir, file.Name())
+
+		sqlPath := filepath.Join(schemaDir, sqlFileName)
+
+		timestamp := time.Now().UTC().Format(time.RFC3339)
+
+		err = seedTable(tx, csvPath, sqlPath, s, timestamp)
+		if err != nil {
+			return err
+		}
+	}
+	return tx.Commit()
+}
+
 /*
 Initializes or opens the database.
 If initializing:
@@ -280,6 +476,11 @@ func DatabaseInit(filepath string) (*sql.DB, error) {
 
 	// Creates lookup tables and fills them with required information
 	err = databaseCreateLookup(database)
+	if err != nil {
+		return nil, err
+	}
+
+	err = databaseSeedLookup(database)
 	if err != nil {
 		return nil, err
 	}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.26.4
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/jedib0t/go-pretty/v6 v6.8.3
 	modernc.org/sqlite v1.55.0
 )
 
@@ -11,9 +12,12 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/ncruces/go-strftime v1.0.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/sys v0.46.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	modernc.org/libc v1.74.1 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.11.0 // indirect
