cc62f077f0996b8dfdcaf6bb87089d8ad8a55771
w7n <73303644+w7o@users.noreply.github.com>
2026-08-17 23:12:07 -0400

    Moved database structs into their own file database_types.go

--- database_queries.go
+++ database_queries.go
@@ -8,54 +8,6 @@ import (
 	"strconv"
 )
 
-type PokemonSpeciesDBEntry struct {
-	PokemonSpeciesID string // alphanumeric dex numbers
-	PokemonName      string
-	Category         string
-	BaseHappiness    int
-	CaptureRate      int
-	GrowthRateID     int
-	GenderRate       int
-	HatchCounter     int
-	ColorID          int
-	ShapeID          int
-	IsMythical       int
-	IsLegendary      int
-}
-
-type PokemonFormDBEntry struct {
-	PokemonFormID       int
-	FormName            string
-	PokemonSpeciesID    string
-	Height              int
-	Weight              int
-	BaseExperienceYield int
-	StatHP              int
-	StatAttack          int
-	StatDefense         int
-	StatSpecialAttack   int
-	StatSpecialDefense  int
-	StatSpeed           int
-}
-
-type PokemonTypeDBEntry struct {
-	PokemonFormID int
-	Slot          int
-	TypeID        int
-}
-
-type PokemonEggGroupDBEntry struct {
-	PokemonSpeciesID string
-	Slot             int
-	EggGroupID       int
-}
-
-type PokemonEVYieldDBEntry struct {
-	PokemonFormID int
-	StatID        int
-	EVYield       int
-}
-
 func boolToInt(b bool) int {
 	if b {
 		return 1
---
+++ database_types.go
@@ -0,0 +1,59 @@
+package main
+
+type metadataTemplate struct {
+	originID   int
+	importedAt *string // non-user
+	checkedAt  *string // non-user nullable
+	createdAt  *string // user
+	updatedAt  *string // user nullable
+	enabled    int
+	hasID      bool // whether or not ID exists
+}
+
+type PokemonSpeciesDBEntry struct {
+	PokemonSpeciesID string // alphanumeric dex numbers
+	PokemonName      string
+	Category         string
+	BaseHappiness    int
+	CaptureRate      int
+	GrowthRateID     int
+	GenderRate       int
+	HatchCounter     int
+	ColorID          int
+	ShapeID          int
+	IsMythical       int
+	IsLegendary      int
+}
+
+type PokemonFormDBEntry struct {
+	PokemonFormID       int
+	FormName            string
+	PokemonSpeciesID    string
+	Height              int
+	Weight              int
+	BaseExperienceYield int
+	StatHP              int
+	StatAttack          int
+	StatDefense         int
+	StatSpecialAttack   int
+	StatSpecialDefense  int
+	StatSpeed           int
+}
+
+type PokemonTypeDBEntry struct {
+	PokemonFormID int
+	Slot          int
+	TypeID        int
+}
+
+type PokemonEggGroupDBEntry struct {
+	PokemonSpeciesID string
+	Slot             int
+	EggGroupID       int
+}
+
+type PokemonEVYieldDBEntry struct {
+	PokemonFormID int
+	StatID        int
+	EVYield       int
+}
